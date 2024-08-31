ac93e34a41e7b7ca6ef84681c8ec24ed5657f6e4
jclaudio <jv.claudio@bol.com.br>
2024-08-31 19:00:53 -0300

    inicializando Spring Boot

---
+++ PIM_FastHub/src/main/java/com/pim/PIM_FastHub/PimFastHubApplication.java
@@ -0,0 +1,13 @@
+package com.pim.PIM_FastHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PimFastHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PimFastHubApplication.class, args);
+	}
+
+}
---
+++ PIM_FastHub/src/test/java/com/pim/PIM_FastHub/PimFastHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pim.PIM_FastHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PimFastHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
